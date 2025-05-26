f27d1d792c462e53fe3f06fc033e3e1277053e3c
t3m14 <artemon.m174@gmail.com>
2025-05-26 12:41:20 +0300

    я что то понаписал и вроде бы оно работает!

--- alekhin/alekhin/urls.py
+++ alekhin/alekhin/urls.py
@@ -23,7 +23,8 @@ from rest_framework_simplejwt.views import (
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.conf import settings
+from django.conf.urls.static import static
 ...
 
 schema_view = get_schema_view(
@@ -52,4 +53,6 @@ urlpatterns = [
     path('images/', include('images.urls')),
     path('service_types/', include('service_types.urls')),
     path('job_titles/', include('job_titles.urls')),
-]
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
