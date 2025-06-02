7e648e56bdd28335020ce600dcc4cabc28097687
t3m14 <artemon.m174@gmail.com>
2025-06-02 14:34:02 +0500

    db

--- alekhin/alekhin/settings.py
+++ alekhin/alekhin/settings.py
@@ -137,9 +137,10 @@ WSGI_APPLICATION = 'alekhin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Make sure this path is correct
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
