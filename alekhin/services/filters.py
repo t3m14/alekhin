b0e11af00f9004f3fdd8f83e578460f78e44da2b
t3m14 <artemon.m174@gmail.com>
2025-06-02 15:59:26 +0500

    service filtering

--- alekhin/services/filters.py
+++ alekhin/services/filters.py
@@ -3,22 +3,6 @@ from django.db.models import Q
 from .models import Service
 
 class ServiceFilter(django_filters.FilterSet):
-    service_direction = django_filters.CharFilter(method='filter_service_direction')
-    service_type = django_filters.CharFilter(method='filter_service_type')
-    job_title = django_filters.CharFilter(method='filter_job_title')
-    
-    def filter_service_direction(self, queryset, name, value):
-        """Фильтр по service_direction JSON полю"""
-        return queryset.filter(service_direction__icontains=value)
-    
-    def filter_service_type(self, queryset, name, value):
-        """Фильтр по service_type JSON полю"""
-        return queryset.filter(service_type__icontains=value)
-    
-    def filter_job_title(self, queryset, name, value):
-        """Фильтр по job_titles JSON полю"""
-        return queryset.filter(job_titles__icontains=value)
-
-    class Meta:
         model = Service
-        fields = ['service_direction', 'service_type', 'job_title']
+        fields = []
+ 
