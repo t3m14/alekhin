bd25a84485b2b8caa48fa6d4cbbaeb2afe180d5f
t3m14 <artemon.m174@gmail.com>
2025-06-04 15:45:00 +0500

    specialists direction

--- alekhin/specialists/views.py
+++ alekhin/specialists/views.py
@@ -24,6 +24,6 @@ class SpecialistViewSet(viewsets.ModelViewSet):
         directions = self.request.query_params.get('direction', None)
 
         if directions:
-            queryset = queryset.filter(directions=str(directions))
+            queryset = queryset.filter(directions__ixeact=str(directions))
             
         return queryset.distinct()
