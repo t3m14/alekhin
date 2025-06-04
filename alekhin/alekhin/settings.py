66b05c70b9e9b819d85746a6f691ce6c90a212f2
t3m14 <artemon.m174@gmail.com>
2025-06-04 14:39:04 +0500

    token

--- alekhin/alekhin/settings.py
+++ alekhin/alekhin/settings.py
@@ -187,7 +187,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': False,
+    'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
     
     'ALGORITHM': 'HS256',
