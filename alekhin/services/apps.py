a645b0e52350a29fba7377692a2ed13bffe894a8
t3m14 <artemon.m174@gmail.com>
2025-06-05 15:17:00 +0500

    search

--- alekhin/services/apps.py
+++ alekhin/services/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
-
 class ServicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'services'
+    
+    def ready(self):
+        import watson
+        from .models import Service
+        
+        watson.register(Service, fields=('name', 'description', 'service_type', 'service_direction'))
